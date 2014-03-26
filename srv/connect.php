2b7cd9aceef41100585bb8ede218ede18927345a
jacobrs <jacob_gagne@hotmail.com>
2014-03-26 15:37:22 -0400

    added connection script

---
+++ srv/connect.php
@@ -0,0 +1,16 @@
+<?php
+	$username 	= 'lokal';
+	$password 	= 'Airborne#82';
+	$hostname 	= 'lokal.db.12509733.hostedresource.com';
+	
+	global $lokaldb;
+	$lokaldb	= new mysqli($hostname, $username, $password, $username);// or die ('could not connect');
+	/* check connection */
+	if (mysqli_connect_errno()) {
+	    printf("Connect failed: %s\n", mysqli_connect_error());
+	    exit();
+	}
+	else
+		echo 'connected';
+
+?>
