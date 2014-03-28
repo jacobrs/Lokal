6c38afd1c397770d4bae0e86be254eacab457983
benrs <benjaminbarault@gmail.com>
2014-03-28 10:19:02 -0400

    Fixed jacob's errors

--- index.php
+++ index.php
@@ -17,8 +17,32 @@
 	<!--<script src="<?php echo $pathToRoot ?>js/modernizr.js" type="text/javascript" language="javascript"></script>-->
 </head>
 <body>
+<<<<<<< HEAD
 	<div class="full width">
-		<form>
+		<br><br><br><br><br><br>
+		<form 	name = "login-form" 
+				style="padding-top:20px;padding-bottom:15px;background-color:#333333;" 
+				method="POST" action ="<?php echo $pathToRoot;?>srv/login.php">
+			<div class="row">
+			    <div class="large-5 columns">
+			     	<label style="color:#CCCCCC;">Username
+			        	<input id="username" autocomplete="off"
+			        	name="username" type="text" placeholder="Username"/>
+			      	</label>
+			    </div>
+			    <div class="large-5 columns">
+			      	<label style="color:#CCCCCC;">Password
+			        	<input id="password" autocomplete="off"
+			        	name="psswd" type="password" placeholder="Password"/>
+			      	</label>
+			    </div>
+				<div class="large-2 columns" style="margin-top:22px; text-align:center;">
+					<a href="#" onclick="document.forms['login-form'].submit();" style="width:15em;"
+						class="button tiny" id="loginBtn">Login</a>
+				</div>
+			</div>
+		</form>
+			<!--
 			<fieldset>
 				<legend>Log-in</legend>
 				<div class="row">
@@ -39,8 +63,50 @@
 					</div>
 				</div>
 			</fieldset>
-		</form>
+			-->
 	</div>
+=======
+	<br><br><br><br><br><br><br>
+	<form name="login-form" style="padding-top:20px;padding-bottom:15px;background-color:#333333;">
+		<div class="row">
+		    <div class="large-6 columns" style = "max-width:62.5em;">
+		     	<label style="color:#CCCCCC;">Username / Email
+		        	<input id="username" autocomplete="off"
+		        	name="username" type="text" placeholder="Username"/>
+		      	</label>
+		    </div>
+		    <div class="large-5 columns">
+		      	<label style="color:#CCCCCC;">Password
+		        	<input id="password" autocomplete="off"
+		        	name="password" type="password" placeholder="Password"/>
+		      	</label>
+		    </div>
+			<div class="large-2 columns" style="margin-top:15px; text-align:center;" id = "loginBtn">
+				<a href="#" onclick="document.forms['login-form'].submit();"
+					class="button tiny" id="loginBtn">Login</a>
+			</div>
+		</div>
+		<!--
+		<fieldset>
+			<legend>Log-in</legend>
+			<div class="row">
+				<div class="small-6 columns">
+					<label> Username
+						<input type="text" placeholder="Username">
+					</label>
+				</div>
+				<div class="small-6 columns">
+					<label> Password
+						<input type="password" placeholder="Password">
+					</label>
+				</div>
+				<div class="small-2 columns">
+					<a href="#" class="button tiny" id="loginBtn"> Login </a>
+				</div>
+			</div>
+		</fieldset>-->
+	</form>
+>>>>>>> 18c682411f6e756c1a656c674533e14dfca7d9cb
 </body>
 </html>
 <?php
