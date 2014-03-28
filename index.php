d2e79c8b08d73796896d87152e8e3b3a0e563aa6
benrs <benjaminbarault@gmail.com>
2014-03-27 23:04:00 -0400

    As good as I could get it, still kind of sloppy

--- index.php
+++ index.php
@@ -10,30 +10,32 @@
 	<meta charset="utf-8">
 	<title>Log-in Page</title>
 	<link rel="stylesheet" type="text/css" href="<?php echo $pathToRoot ?>css/foundation.min.css">
-	<link rel="stylesheet" type="text/css" href="<?php echo $pathToRoot ?>css/normalize.css">
+	<!--<link rel="stylesheet" type="text/css" href="<?php echo $pathToRoot ?>css/normalize.css">-->
 	<link rel="stylesheet" type="text/css" href="<?php echo $pathToRoot ?>css/login.css">
 	<script src="<?php echo $pathToRoot ?>js/jquery.js" type="text/javascript" language="javascript"></script>
 	<script src="<?php echo $pathToRoot ?>js/foundation.min.js" type="text/javascript" language="javascript"></script>
-	<script src="<?php echo $pathToRoot ?>js/modernizr.js" type="text/javascript" language="javascript"></script>
+	<!--<script src="<?php echo $pathToRoot ?>js/modernizr.js" type="text/javascript" language="javascript"></script>-->
 </head>
 <body>
-	<div class="small-12 columns" id="full">
+	<div class="full width">
 		<form>
 			<fieldset>
 				<legend>Log-in</legend>
 				<div class="row">
-					<div class="small-6 columns">
+					<div class="small-8 columns" id="user">
 						<label> Username
 							<input type="text" placeholder="Username">
 						</label>
 					</div>
-					<div class="small-6 columns">
+					<div class="small-7 columns" id="pass">
 						<label> Password
 							<input type="password" placeholder="Password">
 						</label>
 					</div>
-					<div class="small-2 columns">
-						<a href="#" class="button tiny" id="loginBtn"> Login </a>
+				</div>
+				<div class="row">
+					<div class="large-6 columns" id="lginBtn">
+						<a href="#" class="button medium" id="loginBtn"> Login </a>
 					</div>
 				</div>
 			</fieldset>
