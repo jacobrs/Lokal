ccca6b338cf6f339aed1cf4c9bffe1ae1855567a
benrs <benjaminbarault@gmail.com>
2014-04-02 20:04:22 -0400

    System for user input is in place, just needs extensive testing and overall better looks

--- srv/createUser.php
+++ srv/createUser.php
@@ -3,7 +3,6 @@
 	require($pathToRoot.'srv/connect.php');
 	global $lokaldb;
 	
-	$error = 0;
 	$email = $_POST['Email'];
 	$fname = $_POST['Fname'];
 	$lname = $_POST['Lname'];
@@ -14,47 +13,10 @@
 	
 	$date = $year."-".$month."-".$day;
 	
-	$sql = "SELECT * 
-	        FROM `Customers` 
-			WHERE `Email` = '".$email."'";
+	$sql = "INSERT INTO `Customers`(`Email`, `DOB`, `FirstName`, `LastName`, `RestID`, `Gender`)
+			VALUES ('".$email."', '".$date."', '".$fname."', '".$lname."', 1, '".$gender."')";
 	$lokaldb->select_db('lokal');
 	$result = $lokaldb->query($sql);
-	$numrows = $result->num_rows;
-	
-	if($numrows > 0){
-		$error = 1;
-	}
-	
-	$sql = "SELECT *
-	        FROM `Customers`
-			WHERE `FirstName` = '".$fname."'
-			AND `LastName` = '".$lname."'";
-	$lokaldb->select_db('lokal');
-	$result = $lokaldb->query($sql);
-	$numrows = $result->num_rows;
-	
-	if($numrows > 0){
-		$error = 2;
-	}
-	
-	if($error === 0){
-		$sql = "INSERT INTO `Customers`(`Email`, `DOB`, `FirstName`, `LastName`, `RestID`, `Gender`)
-				VALUES ('".$email."', '".$date."', '".$fname."', '".$lname."', 1, '".$gender."')";
-		$lokaldb->select_db('lokal');
-		$result = $lokaldb->query($sql);
-	}
-	
-	$array = [
-		"email"  => $email,
-		"fname"  => $fname,
-		"lname"  => $lname,
-		"DOB"    => $date,
-		"gender" => $gender,
-		"error"  => $error
-	];
-	$data = json_encode($array);
-	
-	echo $data;
 	
 	// footer includes the database close
 	require($pathToRoot.'includes/footer.php');
