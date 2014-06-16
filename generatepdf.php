91cabc48d573f5eb3b2153fde3d859e5d2dea2cf
GeorgeGrafos <grafosgeorge@gmail.com>
2014-06-16 14:43:40 -0400

    Sort of did the PDF

--- generatepdf.php
+++ generatepdf.php
@@ -1,9 +1,20 @@
 <?php
-require('./srv/fpdf.php');
-
-$pdf = new FPDF();
-$pdf->AddPage();
-$pdf->SetFont('Arial','B','16');
-$pdf->Cell(40,10,'Hello World!');
-$pdf->Output();
+	session_start();
+	$pathToRoot = './'
+	require($pathToRoot.'srv/fpdf.php');
+	require($pathToRoot.'srv/common.php');
+	require($pathToRoot.'srv/connect.php');
+	
+	global $lokaldb;
+	$codeID = $_GET['identifier'];
+	$prep = $lokaldb->prepare('SELECT `Code`, `CustID`, `Exp` FROM `Codes` WHERE `CustID` = ?');
+	$prep->bind_param('i', $codeID);
+	$prep->execute();
+	$prep->store_result();
+	
+	$pdf = new FPDF();
+	$pdf->AddPage();
+	$pdf->SetFont('Arial','B','16');
+	$pdf->Cell(40,10,'Hello World!');
+	$pdf->Output();
 ?>
