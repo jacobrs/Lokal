ea68b4631b477278483d8d7c5adfad3a8c840b45
georgegrafos <grafosgeorge@gmail.com>
2014-10-04 22:03:25 -0400

    PDF now displays customers name, expiry date, and message

--- generatepdf.php
+++ generatepdf.php
@@ -6,15 +6,25 @@
 	require($pathToRoot.'srv/connect.php');
 	
 	global $lokaldb;
-	$codeID = $_GET['identifier'];
-	$prep = $lokaldb->prepare('SELECT `Code`, `CustID`, `Exp` FROM `Codes` WHERE `CustID` = ?');
-	$prep->bind_param('i', $codeID);
+	$code = $_GET['identifier'];
+	$sql = "SELECT r.`Message`, r.`RestName`, c.`FirstName`, c.`LastName`, co.`Exp` 
+			FROM (`Codes` co JOIN `Customers` c ON co.`CustID` = c.`ID`) 
+			JOIN `Restaurants` r ON r.`RestID` = c.`RestID` 
+			WHERE co.`Code` = ?";
+	$prep = $lokaldb->prepare($sql);
+	$prep->bind_result($msg, $rName, $fName, $lName, $exp);
+	$prep->bind_param('s', $code);
 	$prep->execute();
-	$prep->store_result();
+	$prep->fetch();
+	$prep->close();
 	
 	$pdf = new FPDF();
 	$pdf->AddPage();
-	$pdf->SetFont('Arial','B','16');
-	$pdf->Cell(40,10,'Hello World!');
+	$pdf->SetFont('Arial','','12');
+	$pdf->Cell($pdf->GetStringWidth($fName)+3,10,$fName,0,0,'L');
+	$pdf->Cell($pdf->GetStringWidth($lName),10,$lName,0,0,'L');
+	$pdf->Cell(0,10,date('Y-m-d',strtotime($exp)),0,0,'R');
+	$pdf->Ln();
+	$pdf->MultiCell($pdf->w-20,10,$msg);
 	$pdf->Output();
 ?>
